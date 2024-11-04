0376e9149f52491b6997eeb465347ea31ea17b53
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-11-04 09:01:12 -0800

    Add edge runtime to route

--- app/api/sentiment/route.ts
+++ app/api/sentiment/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+export const runtime = "edge";
 
 export async function POST(req: NextRequest) {
     // const body: { randomData: string } = await req.json();
